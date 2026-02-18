85419be85bae540234a5b866e424f95b89cc9f9c
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-02-18 17:48:05 +0000

    Week 1

--- Week_1/deep.py
+++ Week_1/deep.py
@@ -1,5 +1,17 @@
 #https://cs50.harvard.edu/python/psets/1/deep/
 
 
+def ask(prompt: str) -> str:
+    user_input = input(prompt).strip().lower()
+    if user_input in ("42", "forty-two", "forty two"):
+        return "Yes"
+    else:
+        return "No"
 
-input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+
+def main():
+    print(ask("What is the Answer to the Great Question of Life, the Universe, and Everything? "))
+
+
+if __name__ == "__main__":
+    main()
