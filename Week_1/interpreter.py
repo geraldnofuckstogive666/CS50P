b03efc06fa648d2eb2f6f94ad628eb2815105905
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-02-22 10:47:29 +0000

    Week 1

--- Week_1/interpreter.py
+++ Week_1/interpreter.py
@@ -1,2 +1,24 @@
 #https://cs50.harvard.edu/python/psets/1/interpreter/
+def main():
+    expression = input("Expression: ").strip()
+    x, operator, z = expression.split()
 
+    x = int(x)
+    z = int(z)
+
+    if operator == "+":
+        result = x + z
+    elif operator == "-":
+        result = x - z
+    elif operator == "*":
+        result = x * z
+    elif operator == "/":
+        result = x / z
+    else:
+        raise ValueError("Invalid operator")
+
+    print(f"{result:.1f}")
+
+
+if __name__ == "__main__":
+    main()
