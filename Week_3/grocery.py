720228e4b12bdf8dadcc88bfcb28aa562dcc1d3a
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-04 21:50:53 +0800

    Week 3

--- Week_3/grocery.py
+++ Week_3/grocery.py
@@ -1,5 +1,19 @@
 #https://cs50.harvard.edu/python/psets/3/grocery/
 
-"""
-    pass
-"""
+
+def main():
+    grocery_list = {}
+    
+    while True:
+        try:
+            user_input = input().strip().upper()
+            grocery_list[user_input] = grocery_list.get(user_input, 0) + 1
+        except EOFError:
+            break
+        
+    for item in sorted(grocery_list):
+        print(grocery_list[item], item)
+            
+        
+if __name__ == "__main__":
+    main()
