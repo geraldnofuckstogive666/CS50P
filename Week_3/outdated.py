4c9ee2907c066e628e052602b81f0c0ea4d5d0f2
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-05 22:08:32 +0800

    Week 3

--- Week_3/outdated.py
+++ Week_3/outdated.py
@@ -1,5 +1,56 @@
 #https://cs50.harvard.edu/python/psets/3/outdated/
 
-"""
-pass    
-"""
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
+
+def main():
+    while True:
+        date = input("Date: ").strip().title()
+
+        try:
+            print(converter(date))
+            return
+        except ValueError:
+            pass
+
+
+def converter(date):
+    if "/" in date:
+        m, d, y = map(int, date.split("/"))
+
+    else:
+        m, d, y = date.split()
+        m = months.index(m) + 1
+        d = int(d.removesuffix(","))
+        y = int(y)
+
+    if not (1 <= m <= 12):
+        raise ValueError
+
+    if not (1 <= d <= 31):
+        raise ValueError
+
+    return f"{y:04}-{m:02}-{d:02}"
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
