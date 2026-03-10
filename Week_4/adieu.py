0bf12e5f342bb6db0551f9ffd517335fd8654147
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-10 09:39:30 +0000

    Week 4

--- Week_4/adieu.py
+++ Week_4/adieu.py
@@ -1,1 +1,20 @@
 #https://cs50.harvard.edu/python/psets/4/adieu/
+# need: pip install inflect
+import inflect
+
+p = inflect.engine()
+
+def main():
+    names = []
+
+    while True:
+        try:
+            names.append(input("Name: ").strip())
+        except EOFError:
+            break
+
+    print(f"Adieu, adieu, to {p.join(names)}")
+
+
+if __name__ == "__main__":
+    main()
