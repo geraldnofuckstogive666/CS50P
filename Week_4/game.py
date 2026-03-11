247550eb2ddd28387d5d0844fff7ed75d4764b43
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-11 15:25:00 +0000

    Week 4

--- Week_4/game.py
+++ Week_4/game.py
@@ -1,1 +1,42 @@
 #https://cs50.harvard.edu/python/psets/4/game/
+import random
+
+
+def main():
+    while True:
+        try: 
+            n = int(input("Level: "))
+        except ValueError:
+            continue
+        if n < 1:
+            continue
+        
+        break
+
+    lucky_number = random.randint(1, n)
+    guessing_game(lucky_number)
+
+
+
+def guessing_game(n):
+    while True:
+        try:
+            guess = int(input("Guess: "))
+        except ValueError:
+            pass
+
+        if guess == n:
+            print("Just right!")
+            return
+
+        elif guess < n:
+            print("Too small!")    
+
+        else:
+            print("Too large!")
+
+        
+
+
+if __name__ == "__main__":
+    main()
