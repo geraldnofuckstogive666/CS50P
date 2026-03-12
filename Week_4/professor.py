3faefa6f02981fa2fb128f5355a6c7a82b386137
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-12 15:39:24 +0000

    Week 4

--- Week_4/professor.py
+++ Week_4/professor.py
@@ -5,26 +5,31 @@ import random
 
 def main():
     score = 0
-    numbers = generate_integer(get_level())
-    for x, y in numbers:
-        error = 0 
-        while True:
-            print(f'{x} + {y} = ',end='')
+    level = get_level()
+
+    for _ in range(10):
+        x = generate_integer(level)
+        y = generate_integer(level)
+
+        errors = 0
+
+        while errors < 3:
             try:
-                answer = int(input())
+                answer = int(input(f"{x} + {y} = "))
             except ValueError:
-                error += 1
-            
+                print("EEE")
+                errors += 1
+                continue
+
             if answer == x + y:
                 score += 1
                 break
             else:
                 print("EEE")
-                error += 1
-            
-            if error == 3:
-                print(f'{x} + {y} = {x + y}')
-                break
+                errors += 1
+
+        if errors == 3:
+            print(f"{x} + {y} = {x+y}")
 
     print(f"Score: {score}")
 
@@ -33,18 +38,17 @@ def get_level():
     while True:
         try:
             level = int(input("Level: "))
+            if 1 <= level <= 3:
+                return level
         except ValueError:
-            continue
-        if not (1 <= level <= 3):
-            continue
-
-        return level
+            pass
 
 
 def generate_integer(level):
-    x = [random.randint(10 ** (level - 1), 10 ** level - 1) for _ in range(10)]
-    y = [random.randint(10 ** (level - 1), 10 ** level - 1) for _ in range(10)]
-    return list(zip(x, y))
+    if level not in (1, 2, 3):
+        raise ValueError
+
+    return random.randint(10**(level-1), 10**level - 1)
 
 
 if __name__ == "__main__":
