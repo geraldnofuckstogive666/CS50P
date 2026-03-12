76b1a74e6b3fadf679ed4ae67085c514e2c92331
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-12 14:33:26 +0000

    Week 4

--- Week_4/professor.py
+++ Week_4/professor.py
@@ -1,1 +1,24 @@
 #https://cs50.harvard.edu/python/psets/4/professor/
+
+import random
+
+
+def main():
+    pass
+
+
+def get_level():
+    ...
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+i
