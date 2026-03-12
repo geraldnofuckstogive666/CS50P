6bed422db8d54258873cb3903994bd1248535848
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-12 15:14:19 +0000

    Week 4

--- Week_4/professor.py
+++ Week_4/professor.py
@@ -4,21 +4,37 @@ import random
 
 
 def main():
+    score = 0
+    numbers = generate_integer(get_level())
     pass
 
 
-def get_level():
-    ...
 
+
+
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level: "))
+        except ValueError:
+            continue
+        if not (1 <= level <= 3):
+            continue
+
+        return level
 
 def generate_integer(level):
-    ...
+    x = [random.randint(10 ** (level - 1), 10 ** level - 1) for _ in range(10)]
+    y = [random.randint(10 ** (level - 1), 10 ** level - 1) for _ in range(10)]
+    return list(zip(x, y))
+
+
+
+
 
 
 if __name__ == "__main__":
     main()
 
 
-
-
-i
