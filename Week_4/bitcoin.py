e0673d0d45f940840bb7f1e3cdba73321fa16040
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-13 23:47:41 +0800

    Week 4

--- Week_4/bitcoin.py
+++ Week_4/bitcoin.py
@@ -4,32 +4,32 @@
 import sys
 import requests
 
-
 URL = "https://rest.coincap.io/v3/assets/bitcoin?apiKey=3c4f4900f45b12eccee5cb5b5fe821b3b6fb455eae0fc53c9e03c9b3b7996638"
 
 
 def fetch_price(url: str) -> float:
-    response = requests.get(url, timeout=10)
-    response.raise_for_status()
-    response_json = response.json()
-    return float(response_json['data']['priceUsd'])
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return float(data["data"]["priceUsd"])
+    except requests.RequestException:
+        sys.exit("Request error")
 
 
 def main():
-    if len(sys.argv) == 2:
-        try:
-            n = float(sys.argv[1])
-        except ValueError:
-            sys.exit("Command line argument is not a number")
-    else:
+    if len(sys.argv) != 2:
         sys.exit("Missing command-line argument")
 
-    price = fetch_price(URL)
-    if not price:
-        sys.exit('Error')
+    try:
+        n = float(sys.argv[1])
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
 
+    price = fetch_price(URL)
     print(f"${price * n:,.4f}")
 
 
 if __name__ == "__main__":
     main()
+
