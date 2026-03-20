4dcb54b8cb4eaddf2b7a012919f340febe79d5f5
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-20 05:58:42 +0000

    Week 5

--- Week_5/test_bank/test_bank.py
+++ Week_5/test_bank/test_bank.py
@@ -1,4 +1,17 @@
 #https://cs50.harvard.edu/python/psets/5/test_bank/
 
+from bank import value
 
-print("world")
+def main():
+    test_value()
+
+
+def test_value():
+    assert value("Hello, Newman") == 0
+    assert value("hello, mickey") == 0
+    assert value("Hey, aRtuRo") == 20
+    assert value("nihao! neiga") == 100
+
+
+if __name__ == "__main__":
+    main()
