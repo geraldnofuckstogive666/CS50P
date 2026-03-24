dcdc8073fbca1b39a19a9760601c364169cca73b
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-24 13:57:04 +0000

    Week 6

--- Week_6/pizza.py
+++ Week_6/pizza.py
@@ -1,1 +1,31 @@
 #https://cs50.harvard.edu/python/psets/6/pizza/
+import sys
+import csv
+from tabulate import tabulate
+
+
+
+x = len(sys.argv)
+
+if x == 2:
+    if ".csv" not in sys.argv[1]:
+        sys.exit("Not a CSV File")
+    else:
+        filename = sys.argv[1]
+        try:
+            with open(filename, "r" , newline='') as csvfile:
+                contentreader = csv.reader(csvfile)
+
+                print(tabulate(contentreader, headers="firstrow",tablefmt="grid"))
+
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+
+
+elif x < 2:
+    sys.exit("Too few command-line arguments")
+
+
+
+else:
+    sys.exit("Too many command-line arguments")
