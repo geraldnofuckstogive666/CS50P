054bd4d16c36dcc9d2df104671a4e755c962cf00
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-04-03 15:13:52 +0000

    Week 7

--- Week_7/um.py
+++ Week_7/um.py
@@ -1,1 +1,19 @@
 #https://cs50.harvard.edu/python/psets/7/um/
+
+import re
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    num = 0
+    if here := re.findall(r"\bum\b",s, re.IGNORECASE):
+        for _ in here:
+            num += 1
+
+    return num
+
+
+if __name__ == "__main__":
+    main()
