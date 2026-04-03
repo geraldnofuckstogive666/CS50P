16b71a52503722a32ac68d4205a6d8c706f11896
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-04-03 15:09:06 +0000

    Week 7

--- Week_7/watch.py
+++ Week_7/watch.py
@@ -1,1 +1,21 @@
 #https://cs50.harvard.edu/python/psets/7/watch/
+
+import re
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    if s := re.search(r".+?src=\"https?://(?:www.)?youtube\.com/embed/([a-z0-9]+)\".+?",s,flags=re.IGNORECASE):
+        url = s.group(1)
+        return f"https://youtu.be/{url}"
+    else:
+        return None
+
+
+
+
+if __name__ == "__main__":
+    main()
