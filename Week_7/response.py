efae0ba1954e920b7f10b5fe1c610da8c24a9c6c
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-04-03 15:17:29 +0000

    Week 7

--- Week_7/response.py
+++ Week_7/response.py
@@ -1,1 +1,15 @@
 #https://cs50.harvard.edu/python/psets/7/response/
+
+from validator_collection import validators, errors
+
+user = input("What's your email? ")
+
+
+try:
+    email_address = validators.email(user)
+    if email_address:
+        print("Valid")
+
+
+except (errors.EmptyValueError, errors.InvalidEmailError) as e:
+    print("Invalid")
