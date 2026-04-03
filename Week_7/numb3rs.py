6ebdf872cec79581131feed17ff711caa7cc99fb
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-04-03 15:05:33 +0000

    Week 7

--- Week_7/numb3rs.py
+++ Week_7/numb3rs.py
@@ -1,1 +1,17 @@
 #https://cs50.harvard.edu/python/psets/7/numb3rs/
+
+import re
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    if re.search(r"^(([0-9]?)|(1[0-9][0-9]?)|(2[0-4][0-9]?)|(2[5][0-5]?))\.(([0-9]?)|(1[0-9][0-9]?)|(2[0-4][0-9]?)|(2[5][0-5]?))\.(([0-9]?)|(1[0-9][0-9]?)|(2[0-4][0-9]?)|(2[5][0-5]?))\.(([0-9]?)|(1[0-9][0-9]?)|(2[0-4][0-9]?)|(2[5][0-5]?))$", ip):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
