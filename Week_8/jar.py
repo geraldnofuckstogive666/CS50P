a2ce79b9179eee7f17735897f9ed7d9b5ce8f339
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-04-08 15:19:21 +0000

    Week 8

--- Week_8/jar.py
+++ Week_8/jar.py
@@ -1,1 +1,42 @@
 #https://cs50.harvard.edu/python/psets/8/jar/
+
+
+class Jar:
+    def __init__(self,capacity=12):
+        self.capacity = capacity
+        self.size = 0
+    def __str__(self):
+        return f"{"🍪" * self.size}"
+
+    #deposit a cookie coo
+    def deposit(self, n):
+        if (self.size + n) > self.capacity:
+            raise ValueError("Sumosobra ka na par!")
+        self.size += n
+
+    def withdraw(self, n):
+        if n > self.size:
+            raise ValueError("Asa man na nimo kuhaon nga cookie daw bi?")
+        self.size -= n
+
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, value):
+        if value <= 0:
+            raise ValueError
+        self._capacity = value
+
+    @property
+    def size(self):
+        return self._size
+
+
+    @size.setter
+    def size(self, value):
+        if value > self.capacity:
+            raise ValueError
+        self._size = value
